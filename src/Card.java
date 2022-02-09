139cba64e37b5e11dcec3ac5bdf38e638307ea00
kevdawwg <89502963+kevdawwg@users.noreply.github.com>
2022-02-09 09:21:46 -0800

    added card class

--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,38 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 
 /** represents a playing card that can draw itself. */
-public class Card implements Drawable, Updateable{
-    private int x;
+public class Card implements Drawable, Updateable {
+    private int suite;
+    private int value;
+
+    public Card(int suite, int value) {
+        this.suite = suite;
+        this.value = value;
+    }
+
+    public int getSuit() {
+        return this.suite;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    @Override
+    public void update(ActionEvent a) {
+        
+        
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        
+    }
+
+    public int compareTo(Card c){
+		return this.getValue() - c.getValue();
+	}
+	
 
 }
