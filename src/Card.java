66940a5374e84a4c097fd886969574f08c4eda88
kevdawwg <89502963+kevdawwg@users.noreply.github.com>
2022-02-11 09:22:14 -0800

    updated card class with instance variables

--- src/Card.java
+++ src/Card.java
@@ -6,10 +6,12 @@ public class Card implements Drawable, Updateable {
     private int suite;
     private int value;
     private boolean isShowing;
+    private int isBlack;
+    private int
 
     public Card(int suite, int value) {
         this.suite = suite; //0 for spades, 1 for hearts, 2 for diamonds, 3 for clubs
-        this.value = value; //
+        this.value = value; //1 for ace, 2-10 for regular cards, 11-13 for Jack, Queen, King
         this.isShowing = false;
     }
 
@@ -29,14 +31,15 @@ public class Card implements Drawable, Updateable {
 
     @Override
     public void draw(Graphics g) {
-        // if (this.suite == 0) {
-        //     g.drawImage("images/cards/" + "s" + this.value + ".png");
-        // }
+        if (this.suite == 0) {
+            g.drawImage("images/cards/" + "s" + this.value + ".png");
+        }
     }
 
     public int compareTo(Card c){
 		return this.getValue() - c.getValue();
 	}
 	
+
 
 }
