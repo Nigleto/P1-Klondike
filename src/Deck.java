7e23619cf8cf2f4f8e98703795d4ddcaa01d407b
kevdawwg <89502963+kevdawwg@users.noreply.github.com>
2022-02-16 09:41:39 -0800

    sddf

--- src/Deck.java
+++ src/Deck.java
@@ -20,23 +20,23 @@ public class Deck extends Pile{
       }
 
 
-    @Override
-    public void draw(Graphics g) {
-        // TODO Auto-generated method stub
+    // @Override
+    // public void draw(Graphics g) {
+    //     // TODO Auto-generated method stub
         
-    }
+    // }
 
-    @Override
-    public void update(ActionEvent a) {
-        // TODO Auto-generated method stub
+    // @Override
+    // public void update(ActionEvent a) {
+    //     // TODO Auto-generated method stub
         
-    }
+    // }
 
-    @Override
-    public boolean canAddCard(Card c) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+    // @Override
+    // public boolean canAddCard(Card c) {
+    //     // TODO Auto-generated method stub
+    //     return false;
+    // }
     
     
 
